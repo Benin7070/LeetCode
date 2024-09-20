2999f1b999dde9bb5bda56d368d14b1db03d65cc
Benin7070 <161836580+Benin7070@users.noreply.github.com>
2024-09-20 23:25:07 +0530

    Time: 37 ms (61.01%), Space: 16.7 MB (30.16%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,7 +4,6 @@ class Solution:
             return ""  
         prefix = ""
         min_length = min(len(s) for s in strs)  
-        
         for i in range(min_length):
             char = strs[0][i] 
             for j in range(1, len(strs)):
