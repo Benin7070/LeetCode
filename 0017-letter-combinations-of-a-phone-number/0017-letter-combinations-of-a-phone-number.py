a5a0ff388544c553f7292886f6ddbb3957680431
Benin <161836580+Benin7070@users.noreply.github.com>
2025-12-04 23:16:01 +0530

    Time: 0 ms (100%), Space: 17.7 MB (91.54%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,7 @@
+from itertools import product
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        nums={2:['a','b','c'],3:['d','e','f'],4:['g','h','i'],5:['j','k','l'],6:['m','n','o'],7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z']}
+        li=[nums[int(i)] for i in digits]
+        res=["".join(p) for p in product(*li)]
+        return res
