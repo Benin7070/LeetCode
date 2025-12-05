4729c906ac4db9333f1079870b61bd0e810dbd08
Benin <161836580+Benin7070@users.noreply.github.com>
2025-12-05 23:18:32 +0530

    Time: 3 ms (80.35%), Space: 17.6 MB (98.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,7 +3,6 @@ class Solution:
         roman_chr={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         res=0
         for i in range(len(s)):
-            print(i)
             if i+1<len(s) and roman_chr[s[i]]<roman_chr[s[i+1]]:
                 res-=roman_chr[s[i]]
             else:
