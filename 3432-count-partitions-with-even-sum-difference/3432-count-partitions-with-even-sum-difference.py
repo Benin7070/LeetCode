bc396eb893f5dc6e59a1bae410ff7a1bcc6a8f30
Benin <161836580+Benin7070@users.noreply.github.com>
2025-12-05 23:06:33 +0530

    Time: 3 ms (26.38%), Space: 17.8 MB (69.5%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
@@ -1,8 +1,9 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
         count=0
+        n=len(nums)
         for i in range(1,len(nums)):
-            if(abs(sum(nums[0:i])-sum(nums[i:len(nums)]))%2==0):
+            if(abs(sum(nums[0:i])-sum(nums[i:n]))%2==0):
                 count+=1
         return count
         
