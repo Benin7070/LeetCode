a5062f3db1f6847bc43f0114cbfc151765c14971
Benin <161836580+Benin7070@users.noreply.github.com>
2025-12-20 20:55:38 +0530

    Time: 7 ms (88.54%), Space: 17.3 MB (99.98%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,15 +2,16 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows==1:
             return s
+        n=len(s)
         res=""
         cycle=2*numRows-2
         for r in range(numRows):
             idx=r
-            while idx<len(s):
+            while idx<n:
                 res+=s[idx]
                 if r!=0 and r!=numRows-1:
                     up_idx=idx+2*(numRows-r-1)
-                    if up_idx<len(s):
+                    if up_idx<n:
                         res+=s[up_idx]
                 idx+=cycle
         return res
