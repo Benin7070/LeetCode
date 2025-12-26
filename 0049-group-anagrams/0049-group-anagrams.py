3554ff08f8173205f730c29e00ab05594733ff86
Benin <161836580+Benin7070@users.noreply.github.com>
2025-12-26 20:18:39 +0530

    Time: 3522 ms (5.02%), Space: 19.4 MB (99.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,39 +1,40 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res=[]
-        i=0
         while strs:
             curr_res=[]
             c={}
-            for j,char in enumerate(strs[i]):
+            for char in strs[0]:
                 if char not in c:
                     c[char]=1
                 else:
                     c[char]+=1
-            curr_res.append(strs[i])
-            el_l=len(strs[i])
-            strs.pop(i)
+            curr_res.append(strs[0])
+            curr_el=sorted(strs[0])
+            curr_el_ln=len(strs[0])
+            strs.pop(0)
 
             for el in strs:
-                tmp=c.copy()
-                f=1
-                if el_l != len(el):
+                if curr_el_ln != len(el):
                     f=0
                     continue
-                for j,char in enumerate(el):
-                    if char not in tmp or tmp[char]<=0:
-                        f=0
-                        break
-                    else:
-                        tmp[char]-=1
-                if f==1:
+                if sorted(el)==curr_el:
                     curr_res.append(el)
-            curr_res.sort()
+                curr_res.sort()
             res.append(curr_res)
             for ch in curr_res:
                 if ch in strs:
                     strs.remove(ch)
         return res
+        #         for j,char in enumerate(el):
+        #             if char not in tmp or tmp[char]<=0:
+        #                 f=0
+        #                 break
+        #             else:
+        #                 tmp[char]-=1
+        #         if f==1:
+        #             curr_res.append(el)
+            
                     
                 
 
