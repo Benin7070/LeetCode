fa8c43d9fc07e7fa4229207f3dc94dfae5c6bdf3
Benin <161836580+Benin7070@users.noreply.github.com>
2026-03-23 22:28:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -9,10 +9,9 @@ class Solution:
                 mono_dec_stack.pop()
             if not mono_dec_stack:
                 next_greater[i]=-1
-                mono_dec_stack.append(i)
             elif mono_dec_stack and mono_dec_stack[-1]>i:
                 next_greater[i]=mono_dec_stack[-1]
-                mono_dec_stack.append(i)
+            mono_dec_stack.append(i)
             print(mono_dec_stack)
         for i in nums1:
             res.append(next_greater[i])
