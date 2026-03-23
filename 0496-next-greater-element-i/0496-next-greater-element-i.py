8069bbb69cba1a4f6a129575a4a31242648bba0c
Benin <161836580+Benin7070@users.noreply.github.com>
2026-03-23 22:25:26 +0530

    Time: 39 ms (11.08%), Space: 20 MB (22.61%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mono_dec_stack=[]
+        next_greater={}
+        nums2=nums2[::-1]
+        res=[]
+        for i in nums2:
+            while mono_dec_stack and mono_dec_stack[-1]<i:
+                mono_dec_stack.pop()
+            if not mono_dec_stack:
+                next_greater[i]=-1
+                mono_dec_stack.append(i)
+            if mono_dec_stack and mono_dec_stack[-1]>i:
+                next_greater[i]=mono_dec_stack[-1]
+                mono_dec_stack.append(i)
+            print(mono_dec_stack)
+        for i in nums1:
+            res.append(next_greater[i])
+        return res
