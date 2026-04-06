e4f70f21243c5a15d4cb1ca0b726a176d9bb81ea
Benin <161836580+Benin7070@users.noreply.github.com>
2026-04-06 22:47:14 +0530

    Time: 3 ms (82.16%), Space: 22.8 MB (26.44%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,25 +1,20 @@
-from collections import deque
 class MinStack:
-
     def __init__(self):
-        self.container=deque()
-        self.min_con=deque()
-    def push(self, val: int) -> None:
-        self.container.append(val)
-        if not self.min_con or val<=self.min_con[-1]:
-            self.min_con.append(val) 
-    def pop(self) -> None:
-        val=self.container.pop()
-        if val==self.min_con[-1]:
-            self.min_con.pop()
-    def top(self) -> int:
-        return self.container[-1]
-    def getMin(self) -> int:
-        return self.min_con[-1]
+        self.stack = []
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            current_min = self.stack[-1][1]
+            self.stack.append((val, min(val, current_min)))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
