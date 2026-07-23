a137ad876a276e9c0bd909fe3025f6e6778d19f2
Benin <161836580+Benin7070@users.noreply.github.com>
2026-07-23 19:16:10 +0530

    Time: 1798 ms (9.04%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,1 +1,1 @@
-select distinct customer_id, count(visit_id) over(partition by customer_id) as count_no_trans from Visits where visit_id not in (select visit_id from Transactions);
+select distinct customer_id, count(visit_id) as count_no_trans from Visits where visit_id not in (select visit_id from Transactions) group by customer_id;
