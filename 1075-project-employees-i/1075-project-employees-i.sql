fcc347c52401ae10dd1c3f97a35d548e17e1e464
Benin <161836580+Benin7070@users.noreply.github.com>
2026-07-25 20:02:12 +0530

    Time: 554 ms (39.88%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,1 @@
+select t1.project_id,ifnull(round(sum(t2.experience_years)/count(*),2),0) as average_years from Project t1 left join Employee t2 on t1.employee_id=t2.employee_id group by t1.project_id;  
