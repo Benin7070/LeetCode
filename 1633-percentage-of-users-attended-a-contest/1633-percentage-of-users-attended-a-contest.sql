93f42b617523d56a8a9a56eee2a1d5f5e8326035
Benin <161836580+Benin7070@users.noreply.github.com>
2026-07-26 23:37:29 +0530

    Time: 1254 ms (12.39%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,1 +1,1 @@
-select t1.contest_id, round(count(t1.user_id)/(select count(*) from Users) *100.0,2) as percentage from Register t1 left join Users t2 on t1.user_id=t2.user_id group by t1.contest_id order by percentage desc,t1.contest_id;
+select t1.contest_id, round(count(t1.user_id)/(select count(*) from Users) *100.0,2) as percentage from Register t1 group by t1.contest_id order by percentage desc,t1.contest_id;
