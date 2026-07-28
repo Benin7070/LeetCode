02aff946bdedebb4cdb996bf7339d60a0de926dc
Benin <161836580+Benin7070@users.noreply.github.com>
2026-07-28 23:12:27 +0530

    Time: 1484 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,1 @@
+select date_format(trans_date,"%Y-%m") as month, country,count(*) as trans_count, count(case when state="approved" then 1 end) as approved_count, sum(amount) as trans_total_amount, ifnull(sum(case when state="approved" then amount end),0) as approved_total_amount from Transactions group by date_format(trans_date,"%Y-%m"),country;
