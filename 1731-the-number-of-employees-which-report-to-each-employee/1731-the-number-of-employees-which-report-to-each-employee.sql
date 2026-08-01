eaf37775015278659581984c6431a2a5a6609d5d
Benin <161836580+Benin7070@users.noreply.github.com>
2026-08-01 22:42:05 +0530

    Time: 963 ms (7.16%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,1 @@
+select e1.employee_id,e1.name,count(*) as reports_count , round(avg(e2.age)) as average_age from Employees e1 cross join Employees e2 on e1.employee_id=e2.reports_to group by e1.employee_id order by e1.employee_id; 
