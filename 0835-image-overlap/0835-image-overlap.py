21090159680ae6e396135644759dcb582a804abc
Benin <161836580+Benin7070@users.noreply.github.com>
2026-09-13 12:49:41 +0530

    Time: 1246 ms (7.93%), Space: 19.5 MB (78.84%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -2,6 +2,12 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         n=len(img1[0])
         ans=0
+        max_ans=0
+        f=1
+        for i in range(n):
+            for j in range(n):
+                if img2[i][j]==1:
+                    max_ans+=1
 
         for dx in range(0-n+1,n):
             for dy in range(0-n+1,n):
@@ -16,4 +22,7 @@ class Solution:
                                 if img1[x][y]==1:
                                     overlap+=1
                 ans=max(ans,overlap)
+                if ans==max_ans:
+                    return ans
+
         return ans
